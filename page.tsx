68f8f136eb960b6b585ff820b0231d2a6e66661d
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2024-11-10 23:12:01 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='text-5xl'>
+        "Welcome  back to My Bio data"
+      </h1>
+      <h2 className='text-3xl'>
+        Name = Syeda Ayesha Fatima
+
+        Age = 26
+
+        Education = Graducated
+
+        Skills = Devloper
+        
+        Experience = 2 years
+
+
+
+      </h2>
+    
+
+    
+
+
+  
+  
+
+    
+      
+    
+  
+
+    </div>
+  )
+}
+
+export default page
+
